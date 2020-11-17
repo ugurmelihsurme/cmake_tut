981dc08a751653158207c144d28baead5f76eaee
ugurmelihsurme <ugurmelihsurme@gmail.com>
2020-11-17 14:47:48 +0300

    step by step tutorial

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 23 Oct 2020
+ *      Author: Administrator
+ */
+#include <iostream>
+#include <TutorialConfig.h>
+int main (int argc, char *argv[]) {
+
+	if (argc < 2) {
+	    // report version
+	    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+	              << Tutorial_VERSION_MINOR << std::endl;
+	    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+	    return 1;
+	 }
+
+	return 0;
+}
+
+
